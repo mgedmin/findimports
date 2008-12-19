ea51e44c4a3c5f474ef3f6d7ea0e823e5522076b
Marius Gedminas <marius@gedmin.as>
2008-12-19 14:35:31 +0200

    Test doctest parse and syntax error handling.

--- testsuite.py
+++ testsuite.py
@@ -3,23 +3,42 @@
 import unittest
 import doctest
 import os
+import sys
 import tempfile
 import shutil
 import glob
 import linecache
 
 
+class RedirectToStdout(object):
+    """A file-like object that prints to sys.stdout
+
+    A reason to use sys.stderr = RedirectToStdout() instead of assigning
+    sys.stderr = sys.stdout is when sys.stdout is later reassigned to a
+    different object (e.g. the StringIO that doctests use) and you want
+    sys.stderr to always refer to whatever sys.stdout is printing to.
+
+    Not all file methods are implemented, just the ones that were actually
+    needed.
+    """
+
+    def write(self, msg):
+        sys.stdout.write(msg)
+
+
 def setUp(test):
+    test.old_stderr = sys.stderr
+    sys.stderr = RedirectToStdout()
     test.old_cwd = os.getcwd()
     test.tempdir = tempfile.mkdtemp('findimports')
     os.chdir(test.tempdir)
 
 
 def tearDown(test):
+    sys.stderr = test.old_stderr
     os.chdir(test.old_cwd)
     shutil.rmtree(test.tempdir)
     linecache.clearcache()
-
 
 
 def additional_tests(): # hook for setuptools
