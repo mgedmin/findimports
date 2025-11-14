490b3ce66949145b174e9db162c526992303deb5
Marius Gedminas <marius@gedmin.as>
2025-11-14 14:45:19 +0200

    Back to development: 2.7.1

--- findimports.py
+++ findimports.py
@@ -114,7 +114,7 @@ import zipfile
 from operator import attrgetter
 
 
-__version__ = '2.7.0'
+__version__ = '2.7.1.dev0'
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __licence__ = 'MIT'
 __url__ = 'https://github.com/mgedmin/findimports'
